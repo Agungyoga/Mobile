9bc483e0271ff6691f2ae20b3093de4f1558d3e4
Agung Yoga <agungyoga373@gmail.com>
2019-03-21 01:09:36 +0700

    coba

---
+++ app/src/main/java/com/example/inventori/MainActivity.java
@@ -0,0 +1,64 @@
+package com.example.inventori;
+
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Menampilkan halaman Fragment yang pertama kali muncul
+        getFragmentPage(new Home());
+
+        /*Inisialisasi BottomNavigationView beserta listenernya untuk
+         *menangkap setiap kejadian saat salah satu menu item diklik
+         */
+        BottomNavigationView bottomNavigation = findViewById(R.id.bottomNavigationView);
+        bottomNavigation.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                Fragment fragment = null;
+
+                //Menantukan halaman Fragment yang akan tampil
+                switch (item.getItemId()){
+                    case R.id.home:
+                        fragment = new Home();
+                        break;
+
+                    case R.id.person:
+                        fragment = new Profile();
+                        break;
+
+                    case R.id.notidications:
+                        fragment = new Peminjam();
+                        break;
+                }
+                return getFragmentPage(fragment);
+            }
+        });
+    }
+
+    //Menampilkan halaman Fragment
+    private boolean getFragmentPage(Fragment fragment){
+        if (fragment != null){
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.page_container, fragment)
+                    .commit();
+            return true;
+        }
+        return false;
+    }
+}
